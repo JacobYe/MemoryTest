dd3d2378bd4d972d10a02a711d6295f45096a142
JacobYe <jacob.mainac@gmail.com>
2016-06-27 10:23:51 +0800

    Update python script, may need to change again

--- AutoTestCSV-CU.py
+++ AutoTestCSV-CU.py
@@ -6,7 +6,8 @@ import time
 import csv
 
 # CU Result json
-url = "http://192.168.1.125:8081/cu?UniqueID=123&UserID=100&Text1="
+url = "http://192.168.1.27:8081/cu?UniqueID=123&UserID=Test&Text1="
+# url = "http://192.168.1.125:8081/cu?UniqueID=123&UserID=100&Text1="
 
 i = 0
 # 测试文件需命名为Test.txt
@@ -28,27 +29,32 @@ with open(test_file) as f:
             print i, ".", line
             time.sleep(0.5)
             rowList = [Input]
-            for j in range(len(memory)):
-                print "---Output %d:---" % (j + 1)
-                print "type: %s" % memory[j]["type"]
-                print "subject: %s" % memory[j]["subject"]
-                print "relation: %s" % memory[j]["relation"]
-                print "entity: %s" % memory[j]["entity"]
-                print "score: %s" % memory[j]["score"]
-                print "candidateAnswer: %s" % memory[j]["candidateAnswer"]
-                print "conflict: %s" % memory[j]["conflict"]
-                extractor = "type: %s\n" % memory[j]["type"]
-                extractor += "subject: %s\n" % memory[j]["subject"]
-                extractor += "relation: %s\n" % memory[j]["relation"]
-                if memory[j]["entity"] is None:
-                    extractor += "entity: %s\n" % memory[j]["entity"]
-                else:
-                    extractor += "entity: %s\n" % memory[j]["entity"]
-                extractor += "score: %s\n" % memory[j]["score"]
-                if memory[j]["candidateAnswer"] is None:
-                    extractor += "candidateAnswer: %s\n" % memory[j]["candidateAnswer"]
-                else:
-                    extractor += "candidateAnswer: %s\n" % memory[j]["candidateAnswer"]
-                extractor += "conflict: %s\n" % memory[j]["conflict"]
-                rowList.append(extractor.encode('utf8'))
+            print memory
+            if memory is None:
+                pass
+                # writer.writerow(rowList)
+            else:
+                for j in range(len(memory)):
+                    print "---Output %d:---" % (j + 1)
+                    print "type: %s" % memory[j]["type"]
+                    print "subject: %s" % memory[j]["subject"]
+                    print "relation: %s" % memory[j]["relation"]
+                    print "entity: %s" % memory[j]["entity"]
+                    print "score: %s" % memory[j]["score"]
+                    print "candidateAnswer: %s" % memory[j]["candidateAnswer"]
+                    print "conflict: %s" % memory[j]["conflict"]
+                    extractor = "type: %s\n" % memory[j]["type"]
+                    extractor += "subject: %s\n" % memory[j]["subject"]
+                    extractor += "relation: %s\n" % memory[j]["relation"]
+                    if memory[j]["entity"] is None:
+                        extractor += "entity: %s\n" % memory[j]["entity"]
+                    else:
+                        extractor += "entity: %s\n" % memory[j]["entity"]
+                    extractor += "score: %s\n" % memory[j]["score"]
+                    if memory[j]["candidateAnswer"] is None:
+                        extractor += "candidateAnswer: %s\n" % memory[j]["candidateAnswer"]
+                    else:
+                        extractor += "candidateAnswer: %s\n" % memory[j]["candidateAnswer"]
+                    extractor += "conflict: %s\n" % memory[j]["conflict"]
+                    rowList.append(extractor.encode('utf8'))
             writer.writerow(rowList)
